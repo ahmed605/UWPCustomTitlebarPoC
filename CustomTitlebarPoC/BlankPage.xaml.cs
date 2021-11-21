d4cbecaa2c248d6c4f1c46cdc910a9ab14338e29
ahmed605 <ahmed.walid605@yahoo.com>
2021-11-21 22:59:35 +0200

    Add project files.

---
+++ CustomTitlebarPoC/BlankPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.WindowManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CustomTitlebarPoC
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage : Page
+    {
+        public BlankPage()
+        {
+            this.InitializeComponent();
+
+            this.Loaded += BlankPage_Loaded;
+        }
+
+        private async void BlankPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Frame appWindowContentFrame = new Frame();
+
+            AppWindow appWindow = await AppWindow.TryCreateAsync();
+            appWindow.TitleBar.SetPreferredVisibility(AppWindowTitleBarVisibility.AlwaysHidden);
+
+            appWindowContentFrame.Navigate(typeof(MainPage));
+            ElementCompositionPreview.SetAppWindowContent(appWindow, appWindowContentFrame);
+
+            await appWindow.TryShowAsync();
+            ((MainPage)appWindowContentFrame.Content).window = appWindow;
+            ((MainPage)appWindowContentFrame.Content).SetTitleBar();
+
+            await ApplicationView.GetForCurrentView().TryConsolidateAsync();
+        }
+    }
+}
---
+++ CustomTitlebarPoC/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Composition;
+using Windows.UI.WindowManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CustomTitlebarPoC
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public AppWindow window;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public void SetTitleBar()
+        {
+            window.Frame.DragRegionVisuals.Add((IVisualElement)TitlebarBase);
+        }
+
+        private async void MinimizeGrid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            IList<AppDiagnosticInfo> infos = await AppDiagnosticInfo.RequestInfoForAppAsync();
+            IList<AppResourceGroupInfo> resourceInfos = infos[0].GetResourceGroups();
+            await resourceInfos[0].StartSuspendAsync();
+        }
+
+        private void CloseGrid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Application.Current.Exit();
+        }
+    }
+}
